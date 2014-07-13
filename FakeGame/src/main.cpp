e9dcb2900670c99805c900d8a169fdc74293f364
Jesse Emond <emond.jesse@gmail.com>
2014-07-12 20:09:31 -0400

    + FageGame first setup

---
+++ FakeGame/src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+template <class T>
+void show(const std::string &name, const T &value);
+
+int main()
+{
+	int health = 100;
+	int mana = 100;
+	signed char gold = 0;
+	long miles = 5;
+	float rate = .1f;
+	double science = .1f;
+
+	while (health > 0) {
+		show("health", health);
+		show("mana", mana);
+		show("gold", gold);
+		show("miles", miles);
+		show("rate", rate);
+		show("science", science);
+
+		cin.get();
+
+		--health;
+		mana -= 2;
+		++gold;
+		miles += 10;
+		rate += .2f;
+		science += .003f;
+	}
+
+	return 0;
+}
+
+template <class T>
+void show(const std::string &name, const T &value)
+{
+	cout << name << "(" << sizeof(T) << "): " << value << endl;
+}
